0016e713839d07458820c3055c3a23743833547e
olgavegh <olgi.vegh@gmail.com>
2024-02-16 10:21:21 +0100

    Basic Frame

---
+++ script.js
@@ -0,0 +1,32 @@
+const rolesElement = document.getElementById("typed-text");
+const cursorElement = document.getElementById("cursor");
+const rolesList = ["webdesigner", "coder", "developer"];
+let roleIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+
+function updateRole() {
+  const currentRole = rolesList[roleIndex];
+  rolesElement.innerHTML = currentRole.substring(0, charIndex);
+
+  if (isDeleting) {
+    charIndex--;
+  } else {
+    charIndex++;
+  }
+
+  if (charIndex === currentRole.length + 1) {
+    isDeleting = true;
+  }
+
+  if (charIndex === 0) {
+    isDeleting = false;
+    roleIndex = (roleIndex + 1) % rolesList.length;
+  }
+}
+
+// Initial role
+updateRole();
+
+// Update role every 100 milliseconds (adjust as needed)
+setInterval(updateRole, 120);
